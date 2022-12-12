6515d181ade89fb830d09dfc2c2b442569d9b0d8
NikDipsakis <nikdipsakis@gmail.com>
2022-12-12 16:13:43 +0200

    new edit

--- ask4.js
+++ ask4.js
@@ -48,36 +48,20 @@ const foodDatabase = [
 /// δεν χρειαζεται να φτιαξεις κλασεις σε αυτην την ασκηση
 
 ///Α) βρες ποια τροφη έχεις τις περισσότερες θερμίδες
-function maxOfMacros() {
-    let maxCal = foodDatabase[0].calories;
-    let maxProt = foodDatabase[0].protein;
-    let maxCarb = foodDatabase[0].carbs;
-    let maxFat = foodDatabase[0].fat;
-    for (i = 0; i < foodDatabase.length; i++) {
-        if (maxCal < foodDatabase[i].calories) {
+function maxOfMacros(foods, macro) {
+    let maxMacro = foods[0][macro];
 
-            maxCal = foodDatabase[i].calories;
-        }
-        if (maxProt < foodDatabase[i].protein) {
+    for (i = 0; i < foods.length; i++) {
+        if (maxMacro < foods[i][macro]) {
 
-            maxProt = foodDatabase[i].protein;
+            maxMacro = foods[i][macro];
         }
-        if (maxCarb < foodDatabase[i].carbs) {
 
-            maxCarb = foodDatabase[i].carbs;
-        }
-        if (maxFat < foodDatabase[i].fat) {
-
-            maxFat = foodDatabase[i].fat;
-        }
     }
-
-    console.log(`the Max Calories : ${maxCal}`);
-    console.log(`the Max Protein : ${maxProt}`);
-    console.log(`the Max Carbs : ${maxCarb}`);
-    console.log(`the Max Fat: ${maxFat}`);
+    console.log(`the Max ${macro}: ${maxMacro}`);
 };
-maxOfMacros();
+maxOfMacros(foodDatabase , ['calories']);
+
 ///Β) κάνε το ίδιο για protein,carbs,fat
 
 ///Γ) ενας άνθρωπος έφαγε 
@@ -89,49 +73,41 @@ const dinner = [foodDatabase[4], foodDatabase[3]]
 const allDay = [...breakfast, ...lunch, ...dinner] // διαβασε τι κανει το ...
 //console.log(...allDay) /// δες τι τυπωνει εδω
 //console.log(...breakfast)
-function totalCalories() {
-    let calorieSum = 0;
-    for (i = 0; i < allDay.length; i++) {
-        calorieSum += allDay[i].calories;
+function totalofMacro(meal, macro) {
+    let macroSum = 0;
+    for (i = 0; i < meal.length; i++) {
+        macroSum += meal[i][macro];
     };
-    return calorieSum;
+    return macroSum;
 };
+totalofMacro(allDay , ['calories']);
 
-function totalProtein() {
-    let proteinSum = 0; for (i = 0; i < allDay.length; i++) {
-        proteinSum += allDay[i].protein;
-    }
-    return proteinSum;
-};
 
-function totalCarbs() {
-    let carbSum = 0;
-    for (i = 0; i < allDay.length; i++) {
-        carbSum += allDay[i].carbs
-    };
-    return carbSum;
-};
+function seeAnalytics(meal) {
+    console.log(`The User consumed ${totalofMacro(meal, ['calories'])} Calories, which contained:\nProtein: ${totalofMacro(meal, ['protein'])} grams\nCarbohydrates: ${totalofMacro(meal, ['carbs'])} grams\nFats: ${totalofMacro(meal, ['fat'])} grams`);
 
-function totalFats() {
-    let fatSum = 0;
-    for (i = 0; i < allDay.length; i++) {
-        fatSum += allDay[i].fat
-    };
-    return fatSum;
-};
-
-function seeAnalytics() {
-    console.log(`The User consumed ${totalCalories()} Calories, which contained:\nProtein: ${totalProtein()} grams\nCarbohydrates: ${totalCarbs()} grams\nFats: ${totalFats()} grams`);
-
-    if (totalCalories() > goal.dayCalories) {
+    if (totalofMacro(meal, ['calories']) > goal.dayCalories) {
         console.log(`The User Exceeded the Calorie Intake`)
     } else {
-        console.log(`${goal.dayCalories - totalCalories()} Calories remain to reach Daily Goal`);
+        console.log(`${goal.dayCalories - totalofMacro(meal, ['calories'])} Calories remain to reach Daily Goal`);
     };
 };
-console.log(seeAnalytics());
 
+seeAnalytics(allDay);
 
+function callByName(foodList , searchName) {
+    for (i = 0; i < foodList.length; i++){
+        //console.log(foodList[i].name)
+        if (foodList[i].name === searchName){
+            console.log(foodList[i]);
+            return
+        }else{
+            console.log(`${searchName} is not registered in this Food List`);
+            return
+        }
+    }
+}
+callByName(foodDatabase , 'egg');
 
 
 // φτιάξε μια συνάρτηση που θα πέρνει σαν είσοδο ενα απο τα απο πανω και θα επιστρέφει την διατροφική αξία του γεύματος
